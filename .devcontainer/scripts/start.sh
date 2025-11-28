bfab1d94fceabdd9634d8f45832da02f626331b4
Luke Collymore <luke@develodesign.co.uk>
2025-11-28 10:54:04 +0000

    Fix condition check for HYVA_LICENCE_KEY

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -186,7 +186,7 @@ else
       php -d memory_limit=-1 bin/magento setup:upgrade
     fi
 
-    if [ "${HYVA_LICENCE_KEY+x}" ]; then
+    if [ "${HYVA_LICENCE_KEY}" ]; then
         echo "**** Configuring Hyvä Theme ****"
         ${COMPOSER_COMMAND} config --auth http-basic.hyva-themes.repo.packagist.com token ${HYVA_LICENCE_KEY}
         ${COMPOSER_COMMAND} config repositories.private-packagist composer https://hyva-themes.repo.packagist.com/${HYVA_PROJECT_NAME}/
